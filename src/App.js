d4119e31f77309f9671d2e29ed925995fa9314b8
KitwanaSh <rodriguekitweze@gmail.com>
2024-01-28 14:26:13 +0000

    Refactor: Replace the home page (temporelly)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './Pages/Home/HomePage';
+// import HomePage from './Pages/Home/HomePage';
+import Dashboard from './Pages/Dashboard/Dashboard'
 import './App.css';
 
 function App() {
-  
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
