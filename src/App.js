2409fbf3bf849e95c629919305fb348704b2e115
Wilfred <Wilychege90@gmail.com>
2024-01-31 08:59:13 +0000

    pay changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/Home/HomePage';
 import './App.css';
+import PricingscreenPage from './Pages/PricingscreenPage';
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/payments" element={<PricingscreenPage />} />
       </Routes>
     </Router>
   );
